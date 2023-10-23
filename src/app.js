730d026a0695832af196e68e23433e438ddbe36f
Montasim <mmamun@mymedicalhub.com>
2023-10-23 19:33:19 +0600

    [ADDED] can not access API from browser

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
 import express from "express";
 import { API_VERSION, PORT } from "./constants/index.js";
+import isBrowserRequest from "./app/middlewares/isBrowserRequest.js";
 import appRoutes from "./app/routes/index.js";
 
 const app = express();
 
 // Middleware to parse JSON requests
 app.use(express.json());
+
+/**
+ * Middleware to handle browser requests.
+ * It checks if the request is made from a browser and prevents access to API endpoints from browsers.
+ *
+ * @param {object} req - The Express request object.
+ * @param {object} res - The Express response object.
+ * @param {function} next - The next middleware function in the pipeline.
+ */
+app.use(isBrowserRequest);
 
 /**
  * Main route that attaches all application routes.
