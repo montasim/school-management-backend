c45484880a1953f7532c9ae84877530b24a07eef
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2023-11-03 00:28:16 +0600

    [UPDATED] download API

--- src/shared/createFolderIfNotExists.js
+++ src/shared/createFolderIfNotExists.js
@@ -1,31 +1,22 @@
 import fs from 'fs';
 import path from 'path';
-import logger from "./logger.js";
 
 /**
- * Creates a folder at the given directory path if it does not exist.
+ * Creates the specified folder if it doesn't exist.
  *
- * @function
- * @async
- * @param {string} directoryName - The path of the directory to be created.
- * @throws Will throw an error if the directory creation fails.
+ * @function createFolderIfNotExists
+ * @param {string} directoryName - The path of the folder to create.
+ * @returns {Promise<void>}
  */
 const createFolderIfNotExists = async (directoryName) => {
     try {
-        // Check if the directory exists
-        if (!fs.existsSync(directoryName)){
-            // Resolve the path to an absolute path
-            const absoluteFolderPath = path.resolve(directoryName);
+        // Resolve the path to an absolute path
+        const absoluteFolderPath = path.resolve(directoryName);
 
-            // Ensure the path exists, creating the directory if necessary.
-            return await fs.promises.mkdir(absoluteFolderPath, { recursive: true });
-        }
+        // Ensure the path exists, creating the directory if necessary.
+        await fs.promises.mkdir(absoluteFolderPath, { recursive: true });
     } catch (error) {
-        // Log the error using the logger
-        logger.error(error);
-
-        // Propagate the error to the calling function
-        throw error;
+        console.error(`Failed to ensure folder exists at ${directoryName}:`, error);
     }
 };
 
