7d9f7e23ff49ba8794c3da9b14eac6f6fd444b1c
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2023-11-05 14:57:16 +0600

    [UPDATED] ensuring a single instance of database connection

--- src/app/middlewares/databaseMiddleware.js
+++ src/app/middlewares/databaseMiddleware.js
@@ -1,57 +1,80 @@
-import { MongoClient, ServerApiVersion } from "mongodb";
+import { MongoClient } from "mongodb";
 import { MONGODB_URI, DATABASE_NAME } from "../../config/config.js";
+import logger from "../../shared/logger.js";
+
+/**
+ * Variable to hold the MongoClient instance.
+ * @type {MongoClient}
+ */
+let client;
+
+/**
+ * Function to get an instance of MongoClient.
+ * Ensures that only a single instance of MongoClient is created and reused.
+ * @returns {Promise<MongoClient>} The MongoClient instance.
+ */
+const getClient = async () => {
+  if (!client) {
+    client = new MongoClient(MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    await client.connect();
+
+    console.info("Database connection successfully established 🚀");
+  }
+
+  return client;
+};
 
 /**
  * Middleware function for connecting to the MongoDB database.
- * Initializes a MongoDB client and establishes a connection to the database.
- * Adds the client and database references to the request object for subsequent use.
- * @param {object} req - Express request object
- * @param {object} res - Express response object
- * @param {function} next - Next middleware function
- * @returns {Promise<void>} Proceeds to the next middleware/controller after establishing the connection
+ * Ensures that the database connection is established and available in the request object.
+ * @param {object} req - Express request object.
+ * @param {object} res - Express response object.
+ * @param {function} next - Next middleware function.
+ * @returns {Promise<void>} Proceeds to the next middleware/controller after establishing the connection.
  */
 const connect = async (req, res, next) => {
   try {
-    // Create a new MongoClient instance with connection options
-    const client = new MongoClient(MONGODB_URI, {
-      serverApi: ServerApiVersion.v1,
-    });
-
-    // Establish a connection to the MongoDB server
-    await client.connect();
-
-    // Attach the client and database references to the request object
+    const client = await getClient();
     req.dbClient = client;
     req.db = client.db(DATABASE_NAME);
-
-    console.info("Database connection successfully established 🚀");
-
-    // Proceed to the next middleware/controller
     next();
   } catch (error) {
     console.error(`Error connecting to database: ${error}`);
+
+    logger.error(error);
   }
 };
 
 /**
  * Middleware function for disconnecting from the MongoDB database.
- * Closes the previously established database connection.
- * @param {object} req - Express request object
- * @returns {Promise<void>} Proceeds to the next middleware/controller after disconnecting from the database
+ * (Optional) Closes the database connection after request processing.
+ * @param {object} req - Express request object.
+ * @param {object} res - Express response object.
+ * @param {function} next - Next middleware function.
+ * @returns {Promise<void>} Proceeds to the next middleware/controller after disconnecting.
  */
-const disconnect = async (req) => {
+const disconnect = async (req, res, next) => {
   try {
     // Close the MongoDB client connection
     await req.dbClient.close();
 
     console.info("Database connection closed");
+
+    next();
   } catch (error) {
     console.error(`Error closing the Database connection: ${error.message}`);
+    
+    logger.error(error);
   }
 };
 
 /**
  * Object containing database-related middleware functions.
+ * @type {object}
  */
 export const DatabaseMiddleware = {
   connect,
