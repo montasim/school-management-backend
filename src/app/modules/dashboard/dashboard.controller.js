bca79df74868df7dfaa3b2c99bab5c2ca2260121
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2023-11-19 19:00:42 +0600

    [UPDATED] summary

--- src/app/modules/dashboard/dashboard.controller.js
+++ src/app/modules/dashboard/dashboard.controller.js
@@ -1,19 +1,44 @@
+/**
+ * @fileoverview Controller for Dashboard Module.
+ *
+ * This file includes controller functions for handling various dashboard-related operations in the application.
+ * The `getSummaryController` function, for instance, is responsible for fetching and returning a summary
+ * of relevant data to the client. These functions typically extract necessary information from the request,
+ * call the appropriate service functions, handle the response, and catch any errors that might occur during the process.
+ *
+ * @requires DashboardService - Service module that contains the business logic for dashboard operations.
+ * @requires extractFromRequest - Helper function to extract data from the request object.
+ * @requires handleServiceResponse - Helper function to handle the response from service functions.
+ * @requires logger - Shared logger utility for logging errors.
+ * @module DashboardController - Exported dashboard controller functions.
+ */
+
 import { DashboardService } from "./dashboard.service.js";
 import extractFromRequest from "../../../helpers/extractFromRequest.js";
 import handleServiceResponse from "../../../helpers/handleServiceResponse.js";
+import logger from "../../../shared/logger.js";
 
 /**
- * @async
- * @function getSummaryListController
- * @description Controller for fetching all Summary.
+ * Controller function for fetching dashboard summary.
+ * This function extracts necessary parameters from the request, calls the getSummaryService from the DashboardService,
+ * handles the service response, and catches any errors during the process.
  *
+ * @async
+ * @function getSummaryController
  * @param {express.Request} req - Express request object.
  * @param {express.Response} res - Express response object to send data back to client.
  */
 const getSummaryController = async (req, res) => {
-    const { adminId, db } = extractFromRequest(req, [], []);
+    try {
+        const { adminId, db } = extractFromRequest(req, [], []);
+        const collectionQuery = req?.query?.for; // Extract 'for' query parameter
 
-    await handleServiceResponse(res, DashboardService.getSummaryService, db, adminId);
+        await handleServiceResponse(res, DashboardService.getSummaryService, db, adminId, collectionQuery);
+    } catch (error) {
+        logger.error(error);
+
+        return error;
+    }
 };
 
 /**
--- src/app/modules/dashboard/dashboard.routes.js
+++ src/app/modules/dashboard/dashboard.routes.js
@@ -1,3 +1,18 @@
+/**
+ * @fileoverview Routes for the Dashboard Module.
+ *
+ * This module sets up the Express router for the dashboard-related routes in the application.
+ * It includes endpoints for fetching system summaries and other dashboard-related data.
+ * The routes are secured with authentication middleware to ensure that only authenticated
+ * users can access them. Each route is associated with a controller function from the
+ * DashboardController module, which contains the logic to handle the specific requests.
+ *
+ * @requires express - Express framework to create router handlers.
+ * @requires authTokenMiddleware - Middleware to authenticate and authorize users.
+ * @requires DashboardController - Controller module for dashboard-related operations.
+ * @module dashboardRoutes - Express router instance for dashboard routes.
+ */
+
 import express from "express";
 import authTokenMiddleware from "../../middlewares/authTokenMiddleware.js";
 import { DashboardController } from "./dashboard.controller.js";
@@ -13,6 +28,8 @@ const router = express.Router();
  *     responses:
  *       200:
  *         description: Summary fetched successfully.
+ *       500:
+ *         description: Internal server error.
  */
 router.get("/summary", [
     authTokenMiddleware,
