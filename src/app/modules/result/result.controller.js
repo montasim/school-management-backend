bf3b81dd8524fbda76f4c9421d34c9d013e39761
Montasim <mmamun@mymedicalhub.com>
2023-11-21 18:21:06 +0600

    [UPDATED] result

--- src/app/modules/result/result.controller.js
+++ src/app/modules/result/result.controller.js
@@ -9,7 +9,7 @@
  *
  * @requires ResultService - Service for handling result-related business logic.
  * @requires extractFromRequest - Helper function for extracting data from the request object.
- * @requires validateTitle - Helper function for validating file title.
+ * @requires validateStringField - Helper function for validating file title.
  * @requires validateUploadedFile - Helper function for validating uploaded files.
  * @requires handleServiceResponse - Helper function for handling responses from services.
  * @requires logger - Shared logging utility.
@@ -19,14 +19,11 @@
 
 import { ResultService } from "./result.service.js";
 import extractFromRequest from "../../../helpers/extractFromRequest.js";
-import validateTitle from "../../../helpers/validateStringField.js";
+import validateStringField from "../../../helpers/validateStringField.js";
 import validateUploadedFile from "../../../helpers/validateUploadedFile.js";
 import handleServiceResponse from "../../../helpers/handleServiceResponse.js";
 import logger from "../../../shared/logger.js";
-import {
-    MAX_PDF_FILE_SIZE,
-    MIME_TYPE_PDF,
-} from "../../../constants/constants.js";
+import { MAX_PDF_FILE_SIZE, MIME_TYPE_PDF } from "../../../constants/constants.js";
 
 /**
  * Asynchronously handles the creation of a new result. This controller extracts result details from the request,
@@ -45,8 +42,9 @@ const createResultController = async (req, res) => {
         const { title, adminId, db } = extractFromRequest(req, ['title']);
         const newResultDetails = { title, adminId };
 
-        await validateTitle(res, title);
-        await validateUploadedFile(res, req.file, MAX_PDF_FILE_SIZE, [MIME_TYPE_PDF]);
+        validateStringField(res, 'title', title, 3, 1000);
+        validateUploadedFile(res, req.file, MAX_PDF_FILE_SIZE, [MIME_TYPE_PDF]);
+
         await handleServiceResponse(res, ResultService.createResultService, db, newResultDetails, req?.file);
     } catch (error) {
         logger.error(error);
