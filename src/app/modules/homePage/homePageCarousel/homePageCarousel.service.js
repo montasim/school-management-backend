b27893dc089d85dc5f932b8d9965f4110935fc3b
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2024-11-10 17:40:03 +0600

    feat: use prisma for /homePage/homePageCarousel api

--- src/app/modules/homePage/homePageCarousel/homePageCarousel.service.js
+++ src/app/modules/homePage/homePageCarousel/homePageCarousel.service.js
@@ -1,21 +1,3 @@
-/**
- * @fileoverview HomePageCarousel Service for Handling HomePageCarousel Data Operations.
- *
- * This module provides services for managing homePageCarousel-related operations in the application.
- * It includes functions for creating, retrieving, updating, and deleting homePageCarousel posts,
- * along with interactions with the Google Drive API for file management.
- * These services abstract the database and file system interactions, providing a
- * clean interface for the controller layer to perform CRUD operations on homePageCarousel data.
- *
- * @requires config - Configuration file for application settings.
- * @requires constants - Application constants for status messages and codes.
- * @requires isValidRequest - Utility function to validate requests.
- * @requires GoogleDriveFileOperations - Helper module for Google Drive file operations.
- * @requires logger - Shared logging utility for error handling.
- * @module HomePageCarouselService - Exported services for homePageCarousel operations.
- */
-
-import { HOME_PAGE_CAROUSEL_COLLECTION_NAME } from "../../../../config/config.js";
 import {
     FORBIDDEN_MESSAGE,
     STATUS_FORBIDDEN,
@@ -25,156 +7,117 @@ import {
     STATUS_UNPROCESSABLE_ENTITY
 } from "../../../../constants/constants.js";
 import { HOME_PAGE_CAROUSEL_CONSTANTS } from "./homePageCarousel.constants.js";
-import isValidRequest from "../../../../shared/isValidRequest.js";
-import { GoogleDriveFileOperations } from "../../../../helpers/GoogleDriveFileOperations.js"
 import logger from "../../../../shared/logger.js";
-import deleteByField from "../../../../shared/deleteByField.js";
-import generateResponseData from "../../../../shared/generateResponseData.js";
-import findByField from "../../../../shared/findByField.js";
-import createByDetails from "../../../../shared/createByDetails.js";
-import getAllData from "../../../../shared/getAllData.js";
-import generateUniqueID from "../../../../helpers/generateUniqueID.js";
+import prisma from "../../../../shared/prisma.js";
 import fileManager from "../../../../helpers/fileManager.js";
+
+import isValidRequest from "../../../../shared/isValidRequest.js";
+import generateResponseData from "../../../../shared/generateResponseData.js";
+import generateUniqueID from "../../../../helpers/generateUniqueID.js";
 import generateFileLink from "../../../../helpers/generateFileLink.js";
 
-/**
- * Creates a new homePageCarousel entry in the database.
- *
- * @async
- * @param {Object} db - Database connection object.
- * @param {Object} newHomePageCarouselDetails - Object containing details of the new homePageCarousel.
- * @param {Object} file - The file object for the homePageCarousel's associated image or content.
- * @returns {Promise<Object>} A promise that resolves to the response object after creating the homePageCarousel.
- */
 const createHomePageCarouselService = async (req, newHomePageCarouselDetails) => {
     try {
         const { db, file, protocol } = req;
         const { title, adminId } = newHomePageCarouselDetails;
 
-        if (!await isValidRequest(db, adminId))
+        if (!await isValidRequest(db, adminId)) {
             return generateResponseData({}, false, STATUS_FORBIDDEN, FORBIDDEN_MESSAGE);
+        }
 
         const uploadFileResponse = await fileManager.uploadFile(file);
         if (!uploadFileResponse?.shareableLink && !uploadFileResponse?.filePath) {
-            return generateResponseData({}, false, STATUS_UNPROCESSABLE_ENTITY, 'Failed to upload in the google drive. Please try again');
+            return generateResponseData({}, false, STATUS_UNPROCESSABLE_ENTITY, 'Failed to upload the file. Please try again');
         }
 
         const fileLink = generateFileLink(req, uploadFileResponse);
         const homePageCarouselDetails = {
             id: generateUniqueID(HOME_PAGE_CAROUSEL_CONSTANTS?.HOME_PAGE_CAROUSEL_ID_PREFIX),
-            title: title,
-            fileId: uploadFileResponse?.fileId,
+            title,
+            fileId: uploadFileResponse.fileId,
             shareableLink: fileLink,
             downloadLink: fileLink,
             createdBy: adminId,
             createdAt: new Date(),
         };
 
-        const result = await createByDetails(db, HOME_PAGE_CAROUSEL_COLLECTION_NAME, homePageCarouselDetails);
-        const latestData = await findByField(db, HOME_PAGE_CAROUSEL_COLLECTION_NAME, 'id', homePageCarouselDetails?.id);
+        const newHomePageCarousel = await prisma.homePageCarousel.create({
+            data: homePageCarouselDetails
+        });
 
-        delete latestData?.createdBy;
-        delete latestData?.modifiedBy;
-        delete latestData?.fileId;
-
-        return result?.acknowledged
-            ? generateResponseData(latestData, true, STATUS_OK, `${title} created successfully`)
-            : generateResponseData({}, false, STATUS_INTERNAL_SERVER_ERROR, 'Failed to create. Please try again');
-
+        return generateResponseData({
+            id: newHomePageCarousel.id,
+            title: newHomePageCarousel.title,
+            shareableLink: newHomePageCarousel.shareableLink,
+            downloadLink: newHomePageCarousel.downloadLink
+        }, true, STATUS_OK, `${title} created successfully`);
     } catch (error) {
         logger.error(error);
-
-        return error;
+        return generateResponseData({}, false, STATUS_INTERNAL_SERVER_ERROR, 'Failed to create. Please try again');
     }
 };
 
-/**
- * Retrieves a list of all homePageHomePageCarousel from the database.
- *
- * @async
- * @param {Object} db - DatabaseMiddleware connection object.
- * @returns {Object} - The list of homePageHomePageCarousel or an error message.
- * @throws {Error} Throws an error if any.
- */
 const getHomePageCarouselListService = async (db) => {
     try {
-        const homePageCarousel = await getAllData(db, HOME_PAGE_CAROUSEL_COLLECTION_NAME);
+        const homePageCarousel = await prisma.homePageCarousel.findMany();
 
-        return homePageCarousel?.length
-            ? generateResponseData(homePageCarousel, true, STATUS_OK, `${homePageCarousel?.length} homePageCarousel found`)
-            : generateResponseData({}, false, STATUS_NOT_FOUND, 'No homePageHomePageCarousel found');
+        return homePageCarousel.length
+            ? generateResponseData(homePageCarousel, true, STATUS_OK, `${homePageCarousel.length} carousel items found`)
+            : generateResponseData({}, false, STATUS_NOT_FOUND, 'No carousel items found');
     } catch (error) {
         logger.error(error);
-
         return error;
     }
 };
 
-/**
- * Retrieves a specific homePageHomePageCarousel by ID from the database.
- *
- * @async
- * @param {Object} db - DatabaseMiddleware connection object.
- * @param {string} homePageCarouselId - The ID of the homePageHomePageCarousel to retrieve.
- * @returns {Object} - The homePageHomePageCarousel details or an error message.
- * @throws {Error} Throws an error if any.
- */
 const getAHomePageCarouselService = async (db, homePageCarouselId) => {
     try {
-        const homePageCarousel = await findByField(db, HOME_PAGE_CAROUSEL_COLLECTION_NAME, 'id', homePageCarouselId);
+        const homePageCarousel = await prisma.homePageCarousel.findUnique({
+            where: { id: homePageCarouselId }
+        });
 
-        delete homePageCarousel?.createdBy;
-        delete homePageCarousel?.modifiedBy;
-        delete homePageCarousel.fileId;
+        if (homePageCarousel) {
+            delete homePageCarousel.createdBy;
+            delete homePageCarousel.modifiedBy;
+            delete homePageCarousel.fileId;
+        }
 
         return homePageCarousel
             ? generateResponseData(homePageCarousel, true, STATUS_OK, `${homePageCarouselId} found successfully`)
             : generateResponseData({}, false, STATUS_NOT_FOUND, `${homePageCarouselId} not found`);
     } catch (error) {
         logger.error(error);
-
         return error;
     }
 };
 
-/**
- * Deletes a specific homePageHomePageCarousel by ID from the database.
- *
- * @async
- * @param {Object} db - DatabaseMiddleware connection object.
- * @param {string} adminId - The user ID making the request.
- * @param {string} homePageCarouselId - The ID of the homePageHomePageCarousel to delete.
- * @returns {Object} - A confirmation message or an error message.
- * @throws {Error} Throws an error if any.
- */
 const deleteAHomePageCarouselService = async (db, adminId, homePageCarouselId) => {
     try {
-        if (!await isValidRequest(db, adminId))
+        if (!await isValidRequest(db, adminId)) {
             return generateResponseData({}, false, STATUS_FORBIDDEN, FORBIDDEN_MESSAGE);
+        }
 
-        const oldDetails = await findByField(db, HOME_PAGE_CAROUSEL_COLLECTION_NAME, 'id', homePageCarouselId);
+        const existingCarousel = await prisma.homePageCarousel.findUnique({
+            where: { id: homePageCarouselId }
+        });
 
-        if (!oldDetails)
+        if (!existingCarousel) {
             return generateResponseData({}, false, STATUS_NOT_FOUND, `${homePageCarouselId} not found`);
-        
-        await fileManager.deleteFile(oldDetails.fileId);
-        
-        const result = await deleteByField(db, HOME_PAGE_CAROUSEL_COLLECTION_NAME, 'id', homePageCarouselId);
+        }
 
-        return result
-            ? generateResponseData({}, true, STATUS_OK, `${homePageCarouselId} deleted successfully`)
-            : generateResponseData({}, false, STATUS_UNPROCESSABLE_ENTITY, `${homePageCarouselId} could not be deleted`);
+        await fileManager.deleteFile(existingCarousel.fileId);
+
+        await prisma.homePageCarousel.delete({
+            where: { id: homePageCarouselId }
+        });
+
+        return generateResponseData({}, true, STATUS_OK, `${homePageCarouselId} deleted successfully`);
     } catch (error) {
         logger.error(error);
-
         return error;
     }
 };
 
-/**
- * @namespace HomePageHomePageCarouselService
- * @description Group of services related to homePageHomePageCarousel operations.
- */
 export const HomePageCarouselService = {
     createHomePageCarouselService,
     getHomePageCarouselListService,
