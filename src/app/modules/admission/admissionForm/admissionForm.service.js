83ee1dc3fc70ad78c2fb483ce918fec089a239ae
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2024-11-10 16:14:14 +0600

    feat: use prisma for /admission/form api

--- src/app/modules/admission/admissionForm/admissionForm.service.js
+++ src/app/modules/admission/admissionForm/admissionForm.service.js
@@ -1,27 +1,3 @@
-/**
- * @fileoverview Services for managing admissionForm operations.
- *
- * This module contains services related to admissionForm operations within the application. It includes functions for
- * creating new admissionForm records, fetching a list of all admissionForms, retrieving specific admissionForms by filename, and
- * deleting admissionForms. Each service function interfaces with the application's database and performs specific
- * business logic operations, ensuring proper management and retrieval of admissionForm data. Additionally, these services
- * handle interactions with external storage services, like Google Drive, for file uploads and deletions.
- *
- * @requires ADMISSION_FORM_COLLECTION_NAME - Configured collection name for admissionForms in the database.
- * @requires constants - Application constants for various status codes and messages.
- * @requires ID_CONSTANTS - Constants for prefixing identifiers in the admissionForm module.
- * @requires isValidRequest - Utility function to validate request authenticity.
- * @requires generateResponseData - Utility function for generating standardized response data.
- * @requires logger - Shared logging utility for error handling.
- * @requires createByDetails - Utility for adding new entries to the database.
- * @requires findByField - Utility for finding a record by its identifier.
- * @requires getAllData - Utility for retrieving all records from a database collection.
- * @requires deleteByField - Utility for deleting records by filename.
- * @requires GoogleDriveFileOperations - Helper for interacting with the Google Drive API.
- * @module AdmissionFormService - Exported object containing admissionForm-related service functions.
- */
-
-import { ADMISSION_FORM_COLLECTION_NAME } from "../../../../config/config.js";
 import {
     FORBIDDEN_MESSAGE,
     STATUS_FORBIDDEN,
@@ -30,28 +6,16 @@ import {
     STATUS_OK,
     STATUS_UNPROCESSABLE_ENTITY
 } from "../../../../constants/constants.js";
+import logger from "../../../../shared/logger.js";
 import { ADMISSION_FORM_CONSTANTS } from "./admissionForm.constants.js";
+import fileManager from "../../../../helpers/fileManager.js";
+import prisma from "../../../../shared/prisma?.js";
+
 import isValidRequest from "../../../../shared/isValidRequest.js";
 import generateResponseData from "../../../../shared/generateResponseData.js";
-import logger from "../../../../shared/logger.js";
-import createByDetails from "../../../../shared/createByDetails.js";
-import findByField from "../../../../shared/findByField.js";
-import getAllData from "../../../../shared/getAllData.js";
-import deleteByField from "../../../../shared/deleteByField.js";
 import generateUniqueID from "../../../../helpers/generateUniqueID.js";
-import fileManager from "../../../../helpers/fileManager.js";
 import generateFileLink from "../../../../helpers/generateFileLink.js";
 
-/**
- * Creates a new admissionForm entry in the database.
- *
- * @async
- * @param {Object} db - DatabaseMiddleware connection object.
- * @param {Object} newAdmissionFormDetails - New admissionForm's details.
- * @param file
- * @returns {Object} - The response after attempting admissionForm creation.
- * @throws {Error} Throws an error if any.
- */
 const createAdmissionFormService = async (req, newAdmissionFormDetails) => {
     try {
         const { db, file, protocol } = req;
@@ -60,19 +24,24 @@ const createAdmissionFormService = async (req, newAdmissionFormDetails) => {
         if (!await isValidRequest(db, adminId))
             return generateResponseData({}, false, STATUS_FORBIDDEN, FORBIDDEN_MESSAGE);
 
-        if (await findByField(db, ADMISSION_FORM_COLLECTION_NAME, 'fileName', file?.originalname))
-            return generateResponseData({}, false, STATUS_UNPROCESSABLE_ENTITY, `File name ${file?.originalname} already exists. Please select a different file name`)
+        const existingForm = await prisma?.admissionForm.findUnique({
+            where: { fileName: file?.originalname }
+        });
+
+        if (existingForm) {
+            return generateResponseData({}, false, STATUS_UNPROCESSABLE_ENTITY, `File name ${file?.originalname} already exists. Please select a different file name`);
+        }
 
         const uploadFileResponse = await fileManager.uploadFile(file);
 
         if (!uploadFileResponse?.shareableLink && !uploadFileResponse?.filePath) {
-            return generateResponseData({}, false, STATUS_UNPROCESSABLE_ENTITY, 'Failed to upload in the google drive. Please try again');
+            return generateResponseData({}, false, STATUS_UNPROCESSABLE_ENTITY, 'Failed to upload to Google Drive. Please try again');
         }
-        
+
         const fileLink = generateFileLink(req, uploadFileResponse);
         const admissionFormDetails = {
             id: generateUniqueID(ADMISSION_FORM_CONSTANTS?.ADMISSION_FORM_ID_PREFIX),
-            title: title,
+            title,
             fileName: file?.originalname,
             fileId: uploadFileResponse?.fileId,
             shareableLink: fileLink,
@@ -81,108 +50,83 @@ const createAdmissionFormService = async (req, newAdmissionFormDetails) => {
             createdAt: new Date(),
         };
 
-        const result = await createByDetails(db, ADMISSION_FORM_COLLECTION_NAME, admissionFormDetails);
-        const latestData = await findByField(db, ADMISSION_FORM_COLLECTION_NAME, 'id', admissionFormDetails?.id);
+        const newAdmissionForm = await prisma?.admissionForm.create({
+            data: admissionFormDetails
+        });
 
-        delete latestData?.createdBy;
-        delete latestData?.fileId;
+        return generateResponseData({
+            id: newAdmissionForm.id,
+            title: newAdmissionForm.title,
+            fileName: newAdmissionForm.fileName,
+            shareableLink: newAdmissionForm.shareableLink,
+            downloadLink: newAdmissionForm.downloadLink
+        }, true, STATUS_OK, `${file?.originalname} uploaded successfully`);
 
-        return result?.acknowledged
-            ? generateResponseData(latestData, true, STATUS_OK, `${file?.originalname} uploaded successfully`)
-            : generateResponseData({}, false, STATUS_INTERNAL_SERVER_ERROR, 'Failed to upload. Please try again');
     } catch (error) {
         logger.error(error);
-
-        return error;
+        return generateResponseData({}, false, STATUS_INTERNAL_SERVER_ERROR, 'Failed to upload. Please try again');
     }
 };
 
-/**
- * Retrieves a list of all admissionForms from the database.
- *
- * @async
- * @param {Object} db - DatabaseMiddleware connection object.
- * @returns {Object} - The list of admissionForms or an error message.
- * @throws {Error} Throws an error if any.
- */
-const getAdmissionFormListService = async (db) => {
+const getAdmissionFormListService = async () => {
     try {
-        const admissionForms = await getAllData(db, ADMISSION_FORM_COLLECTION_NAME);
+        const admissionForms = await prisma?.admissionForm.findMany();
 
-        return admissionForms?.length
-            ? generateResponseData(admissionForms, true, STATUS_OK, `${admissionForms?.length} admissionForm found`)
-            : generateResponseData({}, false, STATUS_NOT_FOUND, 'No admissionForm found');
+        return admissionForms.length
+            ? generateResponseData(admissionForms, true, STATUS_OK, `${admissionForms.length} admission forms found`)
+            : generateResponseData({}, false, STATUS_NOT_FOUND, 'No admission forms found');
     } catch (error) {
         logger.error(error);
-
         return error;
     }
 };
 
-/**
- * Retrieves a specific admissionForm by fileName from the database.
- *
- * @async
- * @param {Object} db - DatabaseMiddleware connection object.
- * @param {string} fileName - The fileName of the admissionForm to retrieve.
- * @returns {Object} - The admissionForm details or an error message.
- * @throws {Error} Throws an error if any.
- */
 const getAAdmissionFormService = async (db, fileName) => {
     try {
-        const admissionForm = await findByField(db, ADMISSION_FORM_COLLECTION_NAME, 'fileName', fileName);
+        const admissionForm = await prisma?.admissionForm.findUnique({
+            where: { fileName }
+        });
 
-        delete admissionForm?.fileId;
+        if (admissionForm) {
+            delete admissionForm.fileId;
+        }
 
         return admissionForm
             ? generateResponseData(admissionForm, true, STATUS_OK, `${fileName} found successfully`)
             : generateResponseData({}, false, STATUS_NOT_FOUND, `${fileName} not found`);
     } catch (error) {
         logger.error(error);
-
         return error;
     }
 };
 
-/**
- * Deletes a specific admissionForm by fileName from the database.
- *
- * @async
- * @param {Object} db - DatabaseMiddleware connection object.
- * @param {string} adminId - The user fileName making the request.
- * @param {string} fileName - The fileName of the admissionForm to delete.
- * @returns {Object} - A confirmation message or an error message.
- * @throws {Error} Throws an error if any.
- */
 const deleteAAdmissionFormService = async (db, adminId, fileName) => {
     try {
-        if (!await isValidRequest(db, adminId))
+        if (!await isValidRequest(db, adminId)) {
             return generateResponseData({}, false, STATUS_FORBIDDEN, FORBIDDEN_MESSAGE);
+        }
 
-        const fileDetails = await findByField(db, ADMISSION_FORM_COLLECTION_NAME, 'fileName', fileName);
+        const fileDetails = await prisma?.admissionForm.findUnique({
+            where: { fileName }
+        });
 
         if (fileDetails) {
             await fileManager.deleteFile(fileDetails.fileId);
 
-            const result = await deleteByField(db, ADMISSION_FORM_COLLECTION_NAME, 'fileName', fileName);
+            await prisma?.admissionForm.delete({
+                where: { fileName }
+            });
 
-            return result
-                ? generateResponseData({}, true, STATUS_OK, `${fileName} deleted successfully`)
-                : generateResponseData({}, false, STATUS_UNPROCESSABLE_ENTITY, `${fileName} could not be deleted`);
+            return generateResponseData({}, true, STATUS_OK, `${fileName} deleted successfully`);
         } else {
             return generateResponseData({}, false, STATUS_NOT_FOUND, `${fileName} not found`);
         }
     } catch (error) {
         logger.error(error);
-
         return error;
     }
 };
 
-/**
- * @namespace AdmissionFormService
- * @description Group of services related to admissionForm operations.
- */
 export const AdmissionFormService = {
     createAdmissionFormService,
     getAdmissionFormListService,
