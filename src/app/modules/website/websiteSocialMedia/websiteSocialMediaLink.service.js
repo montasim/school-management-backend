ba4832850ce41f3141f4fd80f95deb73aced8f99
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2024-11-10 21:05:57 +0600

    feat: use prisma for /website/socialMediaLink api

--- src/app/modules/website/websiteSocialMedia/websiteSocialMediaLink.service.js
+++ src/app/modules/website/websiteSocialMedia/websiteSocialMediaLink.service.js
@@ -1,25 +1,3 @@
-/**
- * @fileoverview Services for Website Important Information Link Operations.
- *
- * This module contains service functions for managing website important information links. These services
- * handle the database interactions and business logic for creating, retrieving, updating, and deleting
- * important information links on a website. Each service function is tailored to process specific data
- * and execute corresponding database operations. This module is integral to managing the data layer of
- * the application, ensuring that website important information links are handled consistently and reliably.
- *
- * @requires DatabaseMiddleware - Middleware for database interactions.
- * @requires ID_CONSTANTS - Constants related to ID generation and validation.
- * @requires generateResponseData - Helper function for generating standardized response data.
- * @requires logger - Utility for logging errors.
- * @requires createByDetails - Helper function for adding new entries to the database.
- * @requires findByField - Helper function for finding database entries by ID.
- * @requires getAllData - Helper function for retrieving all data from a database collection.
- * @requires updateById - Helper function for updating database entries by ID.
- * @requires deleteByField - Helper function for deleting database entries by ID.
- * @module WebsiteSocialMediaLinkService - Exported services for website important information link operations.
- */
-
-import { WEBSITE_SOCIAL_MEDIA_LINK_COLLECTION_NAME } from "../../../../config/config.js";
 import {
     FORBIDDEN_MESSAGE,
     STATUS_FORBIDDEN,
@@ -29,174 +7,112 @@ import {
     STATUS_UNPROCESSABLE_ENTITY
 } from "../../../../constants/constants.js";
 import { ID_CONSTANTS } from "./websiteSocialMediaLink.constants.js";
+import logger from "../../../../shared/logger.js";
+import prisma from "../../../../shared/prisma?.js";
+
 import isValidRequest from "../../../../shared/isValidRequest.js";
 import generateResponseData from "../../../../shared/generateResponseData.js";
-import logger from "../../../../shared/logger.js";
-import createByDetails from "../../../../shared/createByDetails.js";
-import findByField from "../../../../shared/findByField.js";
-import getAllData from "../../../../shared/getAllData.js";
-import updateById from "../../../../shared/updateById.js";
-import deleteByField from "../../../../shared/deleteByField.js";
 import generateUniqueID from "../../../../helpers/generateUniqueID.js";
 
-/**
- * Creates a new entry for a website important information link in the database.
- * Processes the provided link details and adds them to the database. It ensures that the admin ID is valid
- * and then constructs a new entry with the provided link details. The function returns a response indicating
- * successful creation or an error.
- *
- * @param {Object} db - Database connection object.
- * @param {Object} newWebsiteSocialMediaLinkDetails - Details of the new link to be created.
- * @returns {Object} Response indicating the outcome of the operation.
- */
 const createWebsiteSocialMediaLinkService = async (db, newWebsiteSocialMediaLinkDetails) => {
     try {
         const { socialMediaLinkTitle, socialMediaLink, adminId } = newWebsiteSocialMediaLinkDetails;
 
-        if (!await isValidRequest(db, adminId))
+        if (!await isValidRequest(db, adminId)) {
             return generateResponseData({}, false, STATUS_FORBIDDEN, FORBIDDEN_MESSAGE);
+        }
 
         const websiteSocialMediaLinkDetails = {
             id: generateUniqueID(ID_CONSTANTS?.WEBSITE_PREFIX),
-            socialMediaLinkTitle: socialMediaLinkTitle,
-            socialMediaLink: socialMediaLink,
+            socialMediaLinkTitle,
+            socialMediaLink,
             createdBy: adminId,
             createdAt: new Date(),
         };
-        const result = await createByDetails(db, WEBSITE_SOCIAL_MEDIA_LINK_COLLECTION_NAME, websiteSocialMediaLinkDetails);
-        const latestData = await findByField(db, WEBSITE_SOCIAL_MEDIA_LINK_COLLECTION_NAME, 'id', websiteSocialMediaLinkDetails?.id);
 
-        delete latestData?._id;
-        delete latestData?.createdBy;
+        const newLink = await prisma?.websiteSocialMediaLink.create({
+            data: websiteSocialMediaLinkDetails,
+        });
 
-        return result?.acknowledged
-            ? generateResponseData(latestData, true, STATUS_OK, `${latestData?.socialMediaLinkTitle} created successfully`)
-            : generateResponseData({}, false, STATUS_INTERNAL_SERVER_ERROR, 'Failed to create. Please try again');
-
+        return generateResponseData(newLink, true, STATUS_OK, `${newLink.socialMediaLinkTitle} created successfully`);
     } catch (error) {
         logger.error(error);
-
-        return error;
+        return generateResponseData({}, false, STATUS_INTERNAL_SERVER_ERROR, 'Failed to create. Please try again');
     }
 };
 
-/**
- * Retrieves all existing website important information links from the database.
- * Queries the database for all links and returns them, or a message if no links are found.
- *
- * @param {Object} db - Database connection object.
- * @returns {Object} List of links or a message indicating no links found.
- */
 const getWebsiteSocialMediaLinkListService = async (db) => {
     try {
-        const websiteSocialMediaLinks = await getAllData(db, WEBSITE_SOCIAL_MEDIA_LINK_COLLECTION_NAME);
+        const websiteSocialMediaLinks = await prisma?.websiteSocialMediaLink.findMany();
 
-        return websiteSocialMediaLinks?.length
-            ? generateResponseData(websiteSocialMediaLinks, true, STATUS_OK, `${websiteSocialMediaLinks?.length} website social media link found`)
-            : generateResponseData({}, false, STATUS_NOT_FOUND, 'No website social media link found');
+        return websiteSocialMediaLinks.length
+            ? generateResponseData(websiteSocialMediaLinks, true, STATUS_OK, `${websiteSocialMediaLinks.length} social media links found`)
+            : generateResponseData({}, false, STATUS_NOT_FOUND, 'No social media link found');
     } catch (error) {
         logger.error(error);
-
-        return error;
+        return generateResponseData({}, false, STATUS_INTERNAL_SERVER_ERROR, 'Failed to retrieve data. Please try again');
     }
 };
 
-/**
- * Fetches a specific website important information link by its ID.
- * Looks for a link with the provided ID in the database and returns its details, or a message if not found.
- *
- * @param {Object} db - Database connection object.
- * @param {string} websiteSocialMediaLinkId - ID of the link to be retrieved.
- * @returns {Object} Link details or a message indicating link not found.
- */
 const getAWebsiteSocialMediaLinkService = async (db, websiteSocialMediaLinkId) => {
     try {
-        const websiteSocialMediaLink = await findByField(db, WEBSITE_SOCIAL_MEDIA_LINK_COLLECTION_NAME, 'id', websiteSocialMediaLinkId);
+        const websiteSocialMediaLink = await prisma?.websiteSocialMediaLink.findUnique({
+            where: { id: websiteSocialMediaLinkId },
+        });
 
         return websiteSocialMediaLink
             ? generateResponseData(websiteSocialMediaLink, true, STATUS_OK, `${websiteSocialMediaLinkId} found successfully`)
             : generateResponseData({}, false, STATUS_NOT_FOUND, `${websiteSocialMediaLinkId} not found`);
     } catch (error) {
         logger.error(error);
-
-        return error;
+        return generateResponseData({}, false, STATUS_INTERNAL_SERVER_ERROR, 'Failed to retrieve data. Please try again');
     }
 };
 
-/**
- * Updates an existing website important information link in the database.
- * Processes the updated link details and applies the changes to the corresponding entry in the database.
- * Returns a response indicating the outcome of the update operation.
- *
- * @param {Object} db - Database connection object.
- * @param {string} websiteSocialMediaLinkId - ID of the link to be updated.
- * @param {Object} updateWebsiteSocialMediaLinkDetails - Updated link details.
- * @returns {Object} Response indicating the outcome of the update operation.
- */
 const updateAWebsiteSocialMediaLinkService = async (db, websiteSocialMediaLinkId, updateWebsiteSocialMediaLinkDetails) => {
     try {
         const { socialMediaLinkTitle, socialMediaLink, adminId } = updateWebsiteSocialMediaLinkDetails;
 
-        if (!await isValidRequest(db, adminId))
+        if (!await isValidRequest(db, adminId)) {
             return generateResponseData({}, false, STATUS_FORBIDDEN, FORBIDDEN_MESSAGE);
+        }
 
-        if (!await findByField(db, WEBSITE_SOCIAL_MEDIA_LINK_COLLECTION_NAME, 'id', websiteSocialMediaLinkId))
-            return generateResponseData({}, false, STATUS_NOT_FOUND, `${websiteSocialMediaLinkId} not found`);
-
-        const updatedWebsiteSocialMediaLink = {
+        const updatedData = {
             ...(socialMediaLinkTitle && { socialMediaLinkTitle }),
             ...(socialMediaLink && { socialMediaLink }),
             modifiedBy: adminId,
             modifiedAt: new Date(),
         };
-        const result = await updateById(db, WEBSITE_SOCIAL_MEDIA_LINK_COLLECTION_NAME, websiteSocialMediaLinkId, updatedWebsiteSocialMediaLink);
-        const latestData = await findByField(db, WEBSITE_SOCIAL_MEDIA_LINK_COLLECTION_NAME, 'id', websiteSocialMediaLinkId);
 
-        return result?.modifiedCount
-            ? generateResponseData(latestData, true, STATUS_OK, `${websiteSocialMediaLinkId} updated successfully`)
-            : generateResponseData({}, false, STATUS_UNPROCESSABLE_ENTITY, `${websiteSocialMediaLinkId} not updated`);
+        const updatedLink = await prisma?.websiteSocialMediaLink.update({
+            where: { id: websiteSocialMediaLinkId },
+            data: updatedData,
+        });
 
+        return generateResponseData(updatedLink, true, STATUS_OK, `${websiteSocialMediaLinkId} updated successfully`);
     } catch (error) {
         logger.error(error);
-
-        return error;
+        return generateResponseData({}, false, STATUS_INTERNAL_SERVER_ERROR, 'Failed to update data. Please try again');
     }
 };
 
-/**
- * Deletes a specific website important information link from the database.
- * Verifies the admin authority and the existence of the link, then proceeds to delete the link from the database.
- * Returns a response indicating the outcome of the deletion operation.
- *
- * @param {Object} db - Database connection object.
- * @param {string} adminId - Admin ID performing the operation.
- * @param {string} websiteSocialMediaLinkId - ID of the link to be deleted.
- * @returns {Object} Response indicating the outcome of the deletion operation.
- */
 const deleteAWebsiteSocialMediaLinkService = async (db, adminId, websiteSocialMediaLinkId) => {
     try {
-        if (!await isValidRequest(db, adminId))
+        if (!await isValidRequest(db, adminId)) {
             return generateResponseData({}, false, STATUS_FORBIDDEN, FORBIDDEN_MESSAGE);
+        }
 
-        if (!await findByField(db, WEBSITE_SOCIAL_MEDIA_LINK_COLLECTION_NAME, 'id', websiteSocialMediaLinkId))
-            return generateResponseData({}, false, STATUS_NOT_FOUND, `${websiteSocialMediaLinkId} not found`);
+        await prisma?.websiteSocialMediaLink.delete({
+            where: { id: websiteSocialMediaLinkId },
+        });
 
-        const result = await deleteByField(db, WEBSITE_SOCIAL_MEDIA_LINK_COLLECTION_NAME, 'id', websiteSocialMediaLinkId);
-
-        return result
-            ? generateResponseData({}, true, STATUS_OK, `${websiteSocialMediaLinkId} deleted successfully`)
-            : generateResponseData({}, false, STATUS_UNPROCESSABLE_ENTITY, `${websiteSocialMediaLinkId} could not be deleted`);
+        return generateResponseData({}, true, STATUS_OK, `${websiteSocialMediaLinkId} deleted successfully`);
     } catch (error) {
         logger.error(error);
-
-        return error;
+        return generateResponseData({}, false, STATUS_INTERNAL_SERVER_ERROR, 'Failed to delete data. Please try again');
     }
 };
 
-/**
- * @namespace WebsiteSocialMediaLinkService
- * @description Group of services related to website social media link operations.
- */
 export const WebsiteSocialMediaLinkService = {
     createWebsiteSocialMediaLinkService,
     getWebsiteSocialMediaLinkListService,
