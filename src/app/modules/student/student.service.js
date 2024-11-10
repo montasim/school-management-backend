20f85c4f7a8e0de6fd28e6423375d70407a19569
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2024-11-10 23:39:03 +0600

    feat: use prisma for /student api

--- src/app/modules/student/student.service.js
+++ src/app/modules/student/student.service.js
@@ -1,20 +1,3 @@
-/**
- * @fileoverview Student Service for Handling Student Data Operations.
- *
- * This module provides services for managing student-related operations in the application.
- * It includes functions for creating, retrieving, updating, and deleting student posts,
- * along with interactions with the Google Drive API for file management.
- * These services abstract the database and file system interactions, providing a
- * clean interface for the controller layer to perform CRUD operations on student data.
- *
- * @requires config - Configuration file for application settings.
- * @requires constants - Application constants for status messages and codes.
- * @requires isValidRequest - Utility function to validate requests.
- * @requires GoogleDriveFileOperations - Helper module for Google Drive file operations.
- * @requires logger - Shared logging utility for error handling.
- * @module StudentService - Exported services for student operations.
- */
-
 import { LEVEL_COLLECTION_NAME, STUDENT_COLLECTION_NAME } from "../../../config/config.js";
 import {
     FORBIDDEN_MESSAGE, STATUS_BAD_REQUEST,
@@ -25,40 +8,26 @@ import {
     STATUS_UNPROCESSABLE_ENTITY
 } from "../../../constants/constants.js";
 import { STUDENT_CONSTANTS } from "./student.constants.js";
-import isValidRequest from "../../../shared/isValidRequest.js";
-import { GoogleDriveFileOperations } from "../../../helpers/GoogleDriveFileOperations.js"
 import logger from "../../../shared/logger.js";
-import deleteByField from "../../../shared/deleteByField.js";
+import fileManager from "../../../helpers/fileManager.js";
+import prisma from "../../../shared/prisma.js";
+
+import isValidRequest from "../../../shared/isValidRequest.js";
 import generateResponseData from "../../../shared/generateResponseData.js";
 import findByField from "../../../shared/findByField.js";
-import createByDetails from "../../../shared/createByDetails.js";
-import updateById from "../../../shared/updateById.js";
 import generateUniqueID from "../../../helpers/generateUniqueID.js";
-import fileManager from "../../../helpers/fileManager.js";
 import generateFileLink from "../../../helpers/generateFileLink.js";
 
-/**
- * Creates a new student entry in the database.
- *
- * @async
- * @param {Object} db - Database connection object.
- * @param {Object} newStudentDetails - Object containing details of the new student.
- * @param {Object} file - The file object for the student's associated image or content.
- * @returns {Promise<Object>} A promise that resolves to the response object after creating the student.
- */
 const createStudentService = async (req, newStudentDetails) => {
     try {
-        const { db, file, protocol } = req;
+        const { file, protocol } = req;
         const { name, level, adminId } = newStudentDetails;
 
         if (!await isValidRequest(adminId))
             return generateResponseData({}, false, STATUS_FORBIDDEN, FORBIDDEN_MESSAGE);
 
-        const levelExists = await findByField(db, LEVEL_COLLECTION_NAME, 'name', level);
-
-        if (!levelExists) {
-            return generateResponseData({}, false, STATUS_BAD_REQUEST, `Level '${level}' does not exist`);
-        }
+        // Generate custom ID
+        const studentId = generateUniqueID(STUDENT_CONSTANTS.STUDENT_ID_PREFIX);
 
         const uploadFileResponse = await fileManager.uploadFile(file);
         if (!uploadFileResponse?.shareableLink && !uploadFileResponse?.filePath) {
@@ -67,9 +36,9 @@ const createStudentService = async (req, newStudentDetails) => {
 
         const fileLink = generateFileLink(req, uploadFileResponse);
         const studentDetails = {
-            id: generateUniqueID(STUDENT_CONSTANTS?.STUDENT_ID_PREFIX),
-            name: name,
-            level: level,
+            id: studentId, // Use the custom-generated ID
+            name,
+            level,
             fileId: uploadFileResponse?.fileId,
             shareableLink: fileLink,
             downloadLink: fileLink,
@@ -77,184 +46,136 @@ const createStudentService = async (req, newStudentDetails) => {
             createdAt: new Date(),
         };
 
-        const result = await createByDetails(db, STUDENT_COLLECTION_NAME, studentDetails);
-        const latestData = await findByField(db, STUDENT_COLLECTION_NAME, 'id', studentDetails?.id);
+        const result = await prisma.student.create({
+            data: studentDetails
+        });
 
-        delete latestData?._id;
-        delete latestData?.createdBy;
-        delete latestData?.modifiedBy;
-        delete latestData.fileId;
-
-        return result?.acknowledged
-            ? generateResponseData(latestData, true, STATUS_OK, `${name} created successfully`)
-            : generateResponseData({}, false, STATUS_INTERNAL_SERVER_ERROR, 'Failed to create. Please try again');
+        return generateResponseData(result, true, STATUS_OK, `${name} created successfully`);
     } catch (error) {
         logger.error(error);
-
-        return error;
+        return generateResponseData({}, false, STATUS_INTERNAL_SERVER_ERROR, 'Failed to create. Please try again');
     }
 };
 
-/**
- * Retrieves a list of all homePageStudent from the database.
- *
- * @async
- * @param {Object} db - DatabaseMiddleware connection object.
- * @param categoryFilter
- * @returns {Object} - The list of homePageStudent or an error message.
- * @throws {Error} Throws an error if any.
- */
 const getStudentListService = async (db, categoryFilter) => {
     try {
-        let query = {};
-        // Apply the category filter if provided
+        let whereCondition = {};
         if (categoryFilter?.length > 0) {
-            // Use $in operator to match any of the categories
-            query = { 'level': { $in: categoryFilter } };
+            whereCondition.level = { in: categoryFilter };
         }
 
-        // Retrieve and sort administrations from the database based on the query
-        const student = await db?.collection(STUDENT_COLLECTION_NAME)
-            .find(query)
-            .sort({ 'createdAt': -1 }) // Sort by 'createdAt' in descending order
-            .toArray();
+        const students = await prisma.student.findMany({
+            where: whereCondition
+        });
 
-        return student?.length
-            ? generateResponseData(student, true, STATUS_OK, `${student?.length} student found`)
-            : generateResponseData({}, false, STATUS_NOT_FOUND, 'No homePageStudent found');
+        return students.length
+            ? generateResponseData(students, true, STATUS_OK, `${students.length} students found`)
+            : generateResponseData({}, false, STATUS_NOT_FOUND, 'No students found');
     } catch (error) {
         logger.error(error);
-
         return error;
     }
 };
 
-/**
- * Retrieves a specific homePageStudent by ID from the database.
- *
- * @async
- * @param {Object} db - DatabaseMiddleware connection object.
- * @param {string} studentId - The ID of the homePageStudent to retrieve.
- * @returns {Object} - The homePageStudent details or an error message.
- * @throws {Error} Throws an error if any.
- */
 const getAStudentService = async (db, studentId) => {
     try {
-        const student = await findByField(db, STUDENT_COLLECTION_NAME, 'id', studentId);
-
-        delete student?._id;
-        delete student?.createdBy;
-        delete student?.modifiedBy;
-        delete student.fileId;
+        const student = await prisma.student.findUnique({
+            where: { id: studentId },
+        });
 
         return student
             ? generateResponseData(student, true, STATUS_OK, `${studentId} found successfully`)
             : generateResponseData({}, false, STATUS_NOT_FOUND, `${studentId} not found`);
     } catch (error) {
         logger.error(error);
-
         return error;
     }
 };
 
-/**
- * Retrieves a specific homePageStudent by ID from the database.
- *
- * @async
- * @param {Object} db - DatabaseMiddleware connection object.
- * @param {string} studentId - The ID of the homePageStudent to retrieve.
- * @param newStudentDetails
- * @param {Object} file - The file object for the student's associated image or content.
- * @returns {Object} - The homePageStudent details or an error message.
- * @throws {Error} Throws an error if any.
- */
 const updateAStudentService = async (req, studentId, newStudentDetails) => {
     try {
-        const { db, file, protocol } = req;
+        const { file, protocol } = req; // Assuming these are used for file operations and logging.
         const { name, level, adminId } = newStudentDetails;
 
+        // Validate admin ID request
         if (!await isValidRequest(adminId))
             return generateResponseData({}, false, STATUS_FORBIDDEN, FORBIDDEN_MESSAGE);
 
-        // Retrieve the current details of the student
-        const oldDetails = await findByField(db, STUDENT_COLLECTION_NAME, 'id', studentId);
+        // Check if the student exists
+        const oldDetails = await prisma.student.findUnique({
+            where: { id: studentId },
+        });
 
         if (!oldDetails)
             return generateResponseData({}, false, STATUS_NOT_FOUND, `${studentId} not found`);
 
-        // Initialize the object to store updated details
-        const updatedStudentDetails = { ...oldDetails };
+        // Prepare the update object
+        let updateData = {};
 
         // Update name if provided
-        if (name) updatedStudentDetails.name = name;
+        if (name) updateData.name = name;
 
-        // Update level if provided
-        if (level) {
-            if (level === 'level name deleted') {
-                if (oldDetails.level !== 'level name deleted') {
-                    return generateResponseData({}, false, STATUS_BAD_REQUEST, `'level name deleted' cannot be set if another level is present`);
-                }
-            } else {
-                const levelExists = await findByField(db, LEVEL_COLLECTION_NAME, 'name', level);
-
-                if (!levelExists) {
-                    return generateResponseData({}, false, STATUS_BAD_REQUEST, `Level '${level}' does not exist`);
-                }
+        // Update level if provided and validate the level name
+        if (level && level !== oldDetails.level) {
+            // Check if the level name exists or is changed to a valid level
+            const levelExists = await prisma.level.findUnique({ where: { name: level } });
+            if (!levelExists) {
+                return generateResponseData({}, false, STATUS_BAD_REQUEST, `Level '${level}' does not exist`);
             }
-
-            updatedStudentDetails.level = level;
+            updateData.level = level;
         }
 
         // Update file if provided
         if (file) {
-            await fileManager.deleteFile(oldDetails.fileId);
+            // Delete old file if it exists
+            if (oldDetails.fileId) {
+                await fileManager.deleteFile(oldDetails.fileId);
+            }
 
+            // Upload new file and update file details
             const uploadFileResponse = await fileManager.uploadFile(file);
-            if (!uploadFileResponse?.shareableLink && !uploadFileResponse?.filePath)
+            if (!uploadFileResponse?.shareableLink && !uploadFileResponse?.filePath) {
                 return generateResponseData({}, false, STATUS_UNPROCESSABLE_ENTITY, 'File upload failed. Please try again.');
-            
-            const fileLink = generateFileLink(req, uploadFileResponse);
+            }
 
-            updatedStudentDetails.fileId = uploadFileResponse.fileId;
-            updatedStudentDetails.shareableLink = fileLink;
-            updatedStudentDetails.downloadLink = fileLink;
+            // Generate new file links and update in database
+            const fileLink = generateFileLink(req, uploadFileResponse);
+            updateData.fileId = uploadFileResponse.fileId;
+            updateData.shareableLink = fileLink;
+            updateData.downloadLink = fileLink;
         }
 
-        // Update modifiedBy and modifiedAt
-        updatedStudentDetails.modifiedBy = adminId;
-        updatedStudentDetails.modifiedAt = new Date();
+        // Update metadata for modification
+        updateData.modifiedBy = adminId;
+        updateData.modifiedAt = new Date();
 
-        // Update the student data
-        const result = await updateById(db, STUDENT_COLLECTION_NAME, studentId, updatedStudentDetails);
+        // Perform the update operation
+        const result = await prisma.student.update({
+            where: { id: studentId },
+            data: updateData
+        });
 
-        // Retrieve the updated data
-        const latestData = await findByField(db, STUDENT_COLLECTION_NAME, 'id', studentId);
+        // Retrieve the updated data to return updated response
+        const updatedStudent = await prisma.student.findUnique({
+            where: { id: studentId },
+            select: {
+                name: true,
+                level: true,
+                shareableLink: true,
+                downloadLink: true,
+                modifiedBy: true,
+                modifiedAt: true
+                // Add other fields as necessary
+            }
+        });
 
-        // Remove unnecessary data before sending response
-        delete latestData._id;
-        delete latestData.createdBy;
-        delete latestData.modifiedBy;
-        delete latestData.fileId;
-
-        return result?.modifiedCount
-            ? generateResponseData(latestData, true, STATUS_OK, `${studentId} updated successfully`)
-            : generateResponseData({}, false, STATUS_UNPROCESSABLE_ENTITY, `${studentId} not updated`);
+        return generateResponseData(updatedStudent, true, STATUS_OK, `${studentId} updated successfully`);
     } catch (error) {
         logger.error(error);
-        return error;
+        return generateResponseData({}, false, STATUS_INTERNAL_SERVER_ERROR, 'Failed to update. Please try again');
     }
 };
 
-/**
- * Deletes a specific homePageStudent by ID from the database.
- *
- * @async
- * @param {Object} db - DatabaseMiddleware connection object.
- * @param {string} adminId - The user ID making the request.
- * @param {string} studentId - The ID of the homePageStudent to delete.
- * @returns {Object} - A confirmation message or an error message.
- * @throws {Error} Throws an error if any.
- */
 const deleteAStudentService = async (db, adminId, studentId) => {
     try {
         if (!await isValidRequest(adminId))
@@ -267,7 +188,11 @@ const deleteAStudentService = async (db, adminId, studentId) => {
 
         await fileManager.deleteFile(oldDetails.fileId);
 
-        const result = await deleteByField(db, STUDENT_COLLECTION_NAME, 'id', studentId);
+        const result = await prisma.student.delete({
+            where: {
+                id: studentId,
+            },
+        });
 
         return result
             ? generateResponseData({}, true, STATUS_OK, `${studentId} deleted successfully`)
@@ -279,10 +204,6 @@ const deleteAStudentService = async (db, adminId, studentId) => {
     }
 };
 
-/**
- * @namespace HomePageStudentService
- * @description Group of services related to homePageStudent operations.
- */
 export const StudentService = {
     createStudentService,
     getStudentListService,
