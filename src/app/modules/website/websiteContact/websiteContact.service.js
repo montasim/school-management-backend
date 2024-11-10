c3d24320e8e7c0afdd553400b7e505e890e41e2c
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2024-11-10 20:34:43 +0600

    feat: use prisma for /website/contact api

--- src/app/modules/website/websiteContact/websiteContact.service.js
+++ src/app/modules/website/websiteContact/websiteContact.service.js
@@ -1,22 +1,3 @@
-/**
- * @fileoverview Website Contact Services.
- *
- * This file contains the service functions for handling operations related
- * to website contact information. These operations include creating, retrieving,
- * updating, and deleting website contact details. The services interact directly
- * with the database to perform these operations, providing a layer of abstraction
- * between the database and the controllers. Each service function is tailored to
- * handle specific types of operations, ensuring efficient and effective management
- * of website contact data.
- *
- * @requires isValidRequest - Helper function to validate the requester authority.
- * @requires generateResponseData - Utility to format the response data.
- * @requires findByField, createByDetails - Database utility functions.
- * @requires logger - Logger utility for logging information.
- * @module WebsiteContactService - Exports service functions for website contact operations.
- */
-
-import { WEBSITE_CONTACT_COLLECTION_NAME } from "../../../../config/config.js";
 import {
     FORBIDDEN_MESSAGE,
     STATUS_FORBIDDEN,
@@ -26,163 +7,147 @@ import {
     STATUS_UNPROCESSABLE_ENTITY
 } from "../../../../constants/constants.js";
 import { WEBSITE_CONTACT_CONSTANTS } from "./websiteContact.constants.js";
-import isValidRequest from "../../../../shared/isValidRequest.js";
 import logger from "../../../../shared/logger.js";
+import prisma from "../../../../shared/prisma.js";
+
+import isValidRequest from "../../../../shared/isValidRequest.js";
 import generateResponseData from "../../../../shared/generateResponseData.js";
-import findByField from "../../../../shared/findByField.js";
-import createByDetails from "../../../../shared/createByDetails.js";
-import getAllData from "../../../../shared/getAllData.js";
 import generateUniqueID from "../../../../helpers/generateUniqueID.js";
 
-/**
- * Service Function for Creating Website Contact.
- *
- * Manages the creation of new website contact details in the database. Validates
- * if similar contact already exists and if the request is valid before creating
- * new contact details. Returns the created contact information or an error message.
- */
 const createWebsiteContactService = async (db, websiteContactDetails) => {
     try {
         const { address, googleMapLocation, mobile, phone, email, website, adminId } = websiteContactDetails;
-        const existingDetails = await getAllData(db, WEBSITE_CONTACT_COLLECTION_NAME);
 
-        if (existingDetails?.length > 0)
-            return generateResponseData({}, false, STATUS_UNPROCESSABLE_ENTITY, "Website contact already exists. Please update website contact");
-
-        if (!await isValidRequest(db, adminId))
+        if (!await isValidRequest(db, adminId)) {
             return generateResponseData({}, false, STATUS_FORBIDDEN, FORBIDDEN_MESSAGE);
+        }
 
-        const prepareWebsiteDetails = {
+        const existingDetails = await prisma.websiteContact.findMany();
+        if (existingDetails?.length > 0) {
+            return generateResponseData({}, false, STATUS_UNPROCESSABLE_ENTITY, "Website contact already exists. Please update website contact");
+        }
+
+        // Convert googleMapLocation to JSON string
+        const googleMapLocationStr = JSON.stringify(googleMapLocation);
+
+        const contactData = {
             id: generateUniqueID(WEBSITE_CONTACT_CONSTANTS?.WEBSITE_CONTACT_ID_PREFIX),
-            address: address,
-            googleMapLocation: googleMapLocation,
-            mobile: mobile,
-            phone: phone,
-            email: email,
-            website: website,
+            address,
+            googleMapLocation: googleMapLocationStr,
+            mobile,
+            phone,
+            email,
+            website,
             createdBy: adminId,
             createdAt: new Date(),
         };
 
-        const result = await createByDetails(db, WEBSITE_CONTACT_COLLECTION_NAME, prepareWebsiteDetails);
-        const latestData = await findByField(db, WEBSITE_CONTACT_COLLECTION_NAME, 'id', prepareWebsiteDetails?.id);
+        const newContact = await prisma.websiteContact.create({
+            data: contactData,
+        });
 
-        delete latestData?._id;
-        delete latestData?.id;
-        delete latestData?.createdBy;
-        delete latestData?.modifiedBy;
-
-        return result?.acknowledged
-            ? generateResponseData(latestData, true, STATUS_OK, "Website contact added successfully")
-            : generateResponseData({}, false, STATUS_INTERNAL_SERVER_ERROR, 'Failed to create. Please try again');
-
+        return generateResponseData(newContact, true, STATUS_OK, "Website contact added successfully");
     } catch (error) {
         logger.error(error);
-
-        return error;
+        return generateResponseData({}, false, STATUS_INTERNAL_SERVER_ERROR, 'Failed to create. Please try again');
     }
 };
 
-/**
- * Service Function for Retrieving Website Contact.
- *
- * Retrieves the existing website contact details from the database. Formats and
- * returns the contact information or a message indicating if no contact is found.
- */
 const getWebsiteContactService = async (db) => {
     try {
-        const website = await db.collection(WEBSITE_CONTACT_COLLECTION_NAME).findOne({});
+        const websiteContact = await prisma.websiteContact.findFirst();
 
-        delete website?._id;
-        delete website?.id;
-        delete website?.createdBy;
-        delete website?.modifiedBy;
-
-        return website
-            ? generateResponseData(website, true, STATUS_OK, "Website contact found successfully")
-            : generateResponseData({}, false, STATUS_NOT_FOUND, 'No website found');
+        if (websiteContact) {
+            return {
+                data: websiteContact,
+                success: true,
+                status: STATUS_OK,
+                message: "Website contact found successfully"
+            };
+        } else {
+            return {
+                data: {},
+                success: false,
+                status: STATUS_NOT_FOUND,
+                message: 'No website contact found'
+            };
+        }
     } catch (error) {
         logger.error(error);
-
-        return error;
+        throw error;  // Returns the error object instead of formatting
     }
 };
 
-/**
- * Service Function for Updating Website Contact.
- *
- * Handles the updating of existing website contact details in the database.
- * Verifies request validity and updates the contact information based on provided
- * details, returning the updated information or an error response.
- */
 const updateWebsiteContactService = async (db, websiteContactDetails) => {
     try {
-        const { address, googleMapLocation, mobile, phone, email, website, adminId } = websiteContactDetails;
+        const { address, googleMapLocation, mobile, phone, email, website, latitude, longitude, adminId } = websiteContactDetails;
 
-        if (!await isValidRequest(db, adminId))
-            return generateResponseData({}, false, STATUS_FORBIDDEN, FORBIDDEN_MESSAGE);
+        if (!await isValidRequest(db, adminId)) {
+            return {
+                data: {},
+                success: false,
+                status: STATUS_FORBIDDEN,
+                message: FORBIDDEN_MESSAGE
+            };
+        }
 
-        const updatedWebsiteDetails = {
+        const oldDetails = await prisma.websiteContact.findFirst();
+        if (!oldDetails) {
+            return {
+                data: {},
+                success: false,
+                status: STATUS_NOT_FOUND,
+                message: 'Website contact not found'
+            };
+        }
+
+        const updatedData = {
             ...(address && { address }),
-            ...(googleMapLocation && { googleMapLocation }),
             ...(mobile && { mobile }),
             ...(phone && { phone }),
             ...(email && { email }),
             ...(website && { website }),
+            ...(latitude && { latitude }),
+            ...(longitude && { longitude }),
+            // googleMapLocation: googleMapLocation || (latitude && longitude ? `${latitude},${longitude}` : oldDetails.googleMapLocation),
             modifiedBy: adminId,
             modifiedAt: new Date(),
         };
-        const result = await db.collection(WEBSITE_CONTACT_COLLECTION_NAME).findOneAndUpdate(
-            {}, // Assuming you are updating a single document without a filter.
-            { $set: updatedWebsiteDetails },
-            { returnDocument: 'after' } // Returns the updated document
-        );
 
-        if (!result) {
-            return generateResponseData({}, false, STATUS_UNPROCESSABLE_ENTITY, `Website contact not updated`);
-        }
+        const updatedContact = await prisma.websiteContact.update({
+            where: { id: oldDetails.id },
+            data: updatedData,
+        });
 
-        delete result._id;
-        delete result.id;
-        delete result.createdBy;
-        delete result.modifiedBy;
-
-        return generateResponseData(result, true, STATUS_OK, `Website contact updated successfully`);
+        return {
+            data: updatedContact,
+            success: true,
+            status: STATUS_OK,
+            message: "Website contact updated successfully"
+        };
     } catch (error) {
         logger.error(error);
-        return error;
+        throw error;
     }
 };
 
-/**
- * Service Function for Deleting Website Contact.
- *
- * Manages the deletion of website contact details from the database. Validates the
- * request authority and deletes the contact, providing confirmation or an error
- * message upon completion.
- */
 const deleteWebsiteContactService = async (db, adminId) => {
     try {
-        if (!await isValidRequest(db, adminId))
+        if (!await isValidRequest(db, adminId)) {
             return generateResponseData({}, false, STATUS_FORBIDDEN, FORBIDDEN_MESSAGE);
+        }
 
-        // Deletes all documents in the collection without deleting the collection itself
-        const result = await db.collection(WEBSITE_CONTACT_COLLECTION_NAME).deleteMany({});
+        const deleted = await prisma.websiteContact.deleteMany({});
 
-        return result.deletedCount > 0
-            ? generateResponseData({}, true, STATUS_OK, `Website contact deleted successfully`)
-            : generateResponseData({}, false, STATUS_UNPROCESSABLE_ENTITY, `No website contact were found to delete`);
+        return deleted.count > 0
+            ? generateResponseData({}, true, STATUS_OK, "Website contact deleted successfully")
+            : generateResponseData({}, false, STATUS_UNPROCESSABLE_ENTITY, "No website contact found to delete");
     } catch (error) {
         logger.error(error);
-
         return error;
     }
 };
 
-/**
- * @namespace WebsiteService
- * @description Group of services related to website operations.
- */
 export const WebsiteContactService = {
     createWebsiteContactService,
     getWebsiteContactService,
