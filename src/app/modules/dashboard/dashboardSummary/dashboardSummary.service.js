eb9e2ecd3b42cd2ef409ad9f8502e7a0ee1f7a98
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2024-11-11 16:21:25 +0600

    fix: /dashboard/summary api

--- src/app/modules/dashboard/dashboardSummary/dashboardSummary.service.js
+++ src/app/modules/dashboard/dashboardSummary/dashboardSummary.service.js
@@ -1,68 +1,14 @@
-/**
- * @fileoverview Service for Aggregating Dashboard Summary Data.
- *
- * This module provides services for aggregating and fetching summary data from various collections
- * in the database for the dashboard. It includes a primary service function, `getSummaryService`,
- * which dynamically aggregates data from specified collections. This function is designed to be flexible,
- * allowing for aggregation from a single specified collection or multiple collections, based on request parameters.
- * The module ensures that only valid collection names are used for aggregation and handles any errors during
- * the database operation.
- *
- * @requires logger - Shared logging utility for error handling.
- * @requires isValidRequest - Utility function to validate requests.
- * @requires generateResponseData - Utility function to generate standardized response data.
- * @module DashboardService - Exported services for dashboard summary data aggregation.
- */
-
-import {
-    ADMINISTRATION_COLLECTION_NAME,
-    ADMISSION_FORM_COLLECTION_NAME,
-    ADMISSION_INFORMATION_COLLECTION_NAME,
-    ANNOUNCEMENT_COLLECTION_NAME,
-    BLOG_COLLECTION_NAME,
-    CATEGORY_COLLECTION_NAME,
-    DESIGNATION_COLLECTION_NAME,
-    DOWNLOAD_COLLECTION_NAME,
-    HOME_PAGE_CAROUSEL_COLLECTION_NAME,
-    HOME_PAGE_POST_COLLECTION_NAME,
-    LEVEL_COLLECTION_NAME,
-    NOTICE_COLLECTION_NAME,
-    OTHERS_INFORMATION_CATEGORY_COLLECTION_NAME,
-    OTHERS_INFORMATION_COLLECTION_NAME,
-    PHOTO_GALLERY_COLLECTION_NAME,
-    RESULT_COLLECTION_NAME,
-    ROUTINE_COLLECTION_NAME,
-    STUDENT_COLLECTION_NAME,
-    VIDEO_GALLERY_COLLECTION_NAME,
-    WEBSITE_BANNER_COLLECTION_NAME,
-    WEBSITE_CONFIGURATION_COLLECTION_NAME,
-    WEBSITE_CONTACT_COLLECTION_NAME,
-    WEBSITE_IMPORTANT_INFORMATION_LINK_COLLECTION_NAME,
-    WEBSITE_OFFICIAL_LINK_COLLECTION_NAME,
-    WEBSITE_SOCIAL_MEDIA_LINK_COLLECTION_NAME,
-} from "../../../../config/config.js";
 import {
     FORBIDDEN_MESSAGE,
     STATUS_FORBIDDEN,
     STATUS_OK
 } from "../../../../constants/constants.js";
-import isValidRequest from "../../../../shared/isValidRequest.js";
 import logger from "../../../../shared/logger.js";
+import prisma from "../../../../shared/prisma?.js";
+
+import isValidRequest from "../../../../shared/isValidRequest.js";
 import generateResponseData from "../../../../shared/generateResponseData.js";
 
-/**
- * Retrieves and aggregates summary data from specified collections in the database.
- *
- * @async
- * @function getDashboardSummaryService
- * @description Aggregates summary data from various collections in the database based on the request parameters.
- * It supports aggregation from a single specified collection or multiple collections.
- * @param {Object} db - Database connection object.
- * @param {string} adminId - Admin ID for validating the request.
- * @param {string} filterBy - Optional filter to specify a particular collection for aggregation.
- * @returns {Promise<Object>} A promise that resolves to the aggregated summary data or an error message.
- * @throws {Error} If an error occurs during database operation or if invalid parameters are provided.
- */
 const getDashboardSummaryService = async (db, adminId, filterBy) => {
     try {
         if (!(await isValidRequest(adminId))) {
@@ -75,25 +21,31 @@ const getDashboardSummaryService = async (db, adminId, filterBy) => {
         if (filterBy) {
             if (filterBy === 'student') {
                 // Count students only
-                counts[filterBy] = await db.collection(STUDENT_COLLECTION_NAME).find().count();
+                counts[filterBy] = await prisma?.student.count();
             } else {
-                // Count for a specific category in the administration collection
-                counts[filterBy] = await db.collection(ADMINISTRATION_COLLECTION_NAME)
-                    .find({ category: filterBy })
-                    .count();
+                // Count for a specific category in the administration model
+                counts[filterBy] = await prisma?.administration.count({
+                    where: {
+                        category: filterBy
+                    }
+                });
             }
         } else {
             // If no filterBy is provided, return counts for all categories and students
-            const allCategories = await db.collection(ADMINISTRATION_COLLECTION_NAME)
-                .distinct('category');
+            const allCategories = await prisma?.administration.findMany({
+                select: {
+                    category: true
+                },
+                distinct: ['category']
+            });
 
-            for (const category of allCategories) {
-                counts[category] = await db.collection(ADMINISTRATION_COLLECTION_NAME)
-                    .find({ category })
-                    .count();
+            for (const { category } of allCategories) {
+                counts[category] = await prisma?.administration.count({
+                    where: { category }
+                });
             }
 
-            counts['student'] = await db.collection(STUDENT_COLLECTION_NAME).find().count();
+            counts['student'] = await prisma?.student.count();
         }
 
         return generateResponseData(counts, true, STATUS_OK, 'Dashboard summary retrieved successfully');
@@ -104,10 +56,6 @@ const getDashboardSummaryService = async (db, adminId, filterBy) => {
     }
 };
 
-/**
- * @namespace DashboardService
- * @description Group of services related to category operations.
- */
 export const DashboardSummaryService = {
     getDashboardSummaryService,
 };
